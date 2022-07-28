16f672d493a2a990a987bdb910a30f86cf925996
hazelbarcibal <hazelannmae.barcibal@gsfe.tupcavite.edu.ph>
2022-07-28 23:15:10 +0800

    mema na design kemerut

--- djangoproj/inventorysystem/urls.py
+++ djangoproj/inventorysystem/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='inventorysystem-home'),
+    path('supplies-delivery/', views.suppliesDeliver, name='inventorysystem-suppliesDeliver'),
+    path('add-supply-item/', views.addSupply, name='inventorysystem-addSupply'),
 ]
--- djangoproj/inventorysystem/views.py
+++ djangoproj/inventorysystem/views.py
@@ -1,5 +1,12 @@
+import re
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'task/home.html')
+
+def suppliesDeliver(request):
+    return render(request, 'task/supplies-delivery.html')
+
+def addSupply(request):
+    return render(request, 'task/add-supply.html')
