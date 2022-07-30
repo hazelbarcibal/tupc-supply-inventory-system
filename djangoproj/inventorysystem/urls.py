c432c2e4ad98db3d5cdd8d6b99dcd68946c17b38
emielaideligutan <91707627+emielaideligutan@users.noreply.github.com>
2022-07-30 10:38:08 +0800

    Update urls.py

--- djangoproj/inventorysystem/urls.py
+++ djangoproj/inventorysystem/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='inventorysystem-home'),
     path('supplies-delivery/', views.suppliesDeliver, name='inventorysystem-suppliesDeliver'),
     path('add-supply-item/', views.addSupply, name='inventorysystem-addSupply'),
+    path('equipment-delivery/', views.equipmentDeliver, name='inventorysystem-equipmentDeliver'),
+    path('add-equipment-item/', views.addEquipment, name='inventorysystem-addEquipment'),
 ]
