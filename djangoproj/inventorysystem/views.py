4177ef9d48b7f11440bfa0a91468889ce9cc2deb
emielaideligutan <91707627+emielaideligutan@users.noreply.github.com>
2022-08-06 14:02:01 +0800

    Update views.py

--- djangoproj/inventorysystem/views.py
+++ djangoproj/inventorysystem/views.py
@@ -23,5 +23,17 @@ def suppliesWithdraw(request):
 def equipmentWithdraw(request):
     return render(request, 'task/equipment-withdraw.html')
 
+def viewRequestSupply(request):
+    return render(request, 'task/view-request-supplies.html')
+
+def viewRequestEquipment(request):
+    return render(request, 'task/view-request-equipment.html')
+
+def depRequestSupply(request):
+    return render(request, 'task/dep-request-supply.html')
+
+def depRequestEquipment(request):
+    return render(request, 'task/dep-request-equipment.html')
+
 def statusLimit(request):
     return render(request, 'task/status-limit.html')
