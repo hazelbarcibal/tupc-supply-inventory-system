f7e6dc0bd06979f4800584cfb2b5ac288a9f670a
WendelAlvero <wendel.alvero@gsfe.tupcavite.edu.ph>
2022-09-08 00:18:43 +0800

    maliyunguna

--- djangoproj/inventorysystem/views.py
+++ djangoproj/inventorysystem/views.py
@@ -14,6 +14,15 @@ def deptRegister(request):
     return render(request, 'task/department-register.html')
 
 def suppliesDeliver(request):
+    if request.method == "POST":
+        if request.post.get('supply_delivery_itemname') and request.POST.get('supply_delivery_unit') and request.POST.get('supply_delivery_quantity'):
+            update_delivery_record = deliverysupply()
+            update_delivery_record.delivery_supply_itemname = request.POST.get('supply_delivery_item_name')
+            update_delivery_record.delivery_supply_description = request.POST.get('supply_delivery_description')
+            update_delivery_record.delivery_supply_unit = request.POST.get('supply_delivery_unit')
+            update_delivery_record.delivery_supply_quantity= request.POST.get('supply_delivery_quantity')
+            update_delivery_record.delivery_supply_brand = request.POST.get('supply_delivery_brand')
+            update_delivery_record.save()
     return render(request, 'task/supplies-delivery.html')
 
 def addItem(request):
@@ -26,15 +35,6 @@ def equipmentDeliver(request):
     return render(request, 'task/add-equipment.html')
 
 def suppliesWithdraw(request):
-    if request.method == "POST":
-        if request.post.get('supply_delivery_itemname') and request.POST.get('supply_delivery_unit') and request.POST.get('supply_delivery_quantity'):
-            update_delivery_record = deliverysupply()
-            update_delivery_record.delivery_supply_itemname = request.POST.get('supply_delivery_item_name')
-            update_delivery_record.delivery_supply_description = request.POST.get('supply_delivery_description')
-            update_delivery_record.delivery_supply_unit = request.POST.get('supply_delivery_unit')
-            update_delivery_record.delivery_supply_quantity= request.POST.get('supply_delivery_quantity')
-            update_delivery_record.delivery_supply_brand = request.POST.get('supply_delivery_brand')
-            update_delivery_record.save()
     return render(request, 'task/supplies-withdraw.html')
 
 def equipmentWithdraw(request):
