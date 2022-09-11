d93ec5c61f3f98a90764cf1f29a1724f81262423
hazelbarcibal <hazelannmae.barcibal@gsfe.tupcavite.edu.ph>
2022-09-12 01:36:35 +0800

    admin.py register models

---
+++ djangoproj/inventorysystem/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(admin)
+admin.site.register(deliverysupply)
+admin.site.register(deliveryequipment)
+admin.site.register(requestsupply)
+admin.site.register(requestequipment)
+admin.site.register(withdrawsupply)
+admin.site.register(withdrawequipment)
+admin.site.register(returnequipment)
+admin.site.register(limitrecords)
+admin.site.register(supplymainstorage)
+admin.site.register(equipmentmainstorage)
